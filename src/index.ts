a1eafdc5aa12376e4a4d3d24dd349b10cec5391b
Basel Alsayed <bfalsayed@gmail.com>
2021-09-07 01:18:46 +0300

    Add the ability to reach and configure axios instance

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as call } from "./call/call";
 export { default as apiMethod } from "./apiMethod/apiMethod";
-
+export { default as axios } from "axios";
 /* Types And Interfaces */
 export  { PostApiType, GetApiType } from "./call/types";
 
